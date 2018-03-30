d6ef97118621fd21e7d4af0d2edd7d2a6e54e9c5
Brandon Stark <6299956+Brandon-Stark@users.noreply.github.com>
2018-03-30 18:43:51 +0800

    Update 06-loop.R

--- exercises/06-loop.R
+++ exercises/06-loop.R
@@ -4,7 +4,7 @@ weekdays <- c("Sunday", "Monday", "Tuesday",
   "Wednesday", "Thursday", "Friday", "Saturday")
 
 for (i in weekdays) {
-  rmarkdown::render("exercises/06-parameters-answer.Rmd", 
+  rmarkdown::render("exercises/05-parameters-answer.Rmd", 
     params = list(dow = i),
     output_file = paste0("delays_", i, ".html")
   )
